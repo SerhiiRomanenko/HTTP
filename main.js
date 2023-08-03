190978f464bbd60934b0927bb0d13ead8f64ca24
Serhii <79630049+SerhiiRomanenko@users.noreply.github.com>
2023-08-03 16:23:40 +0300

    HTTP

--- main.js
+++ main.js
@@ -1,44 +1,16 @@
-const $container = document.querySelector(".row");
+const $people = document.querySelector(".main__people");
 
-let xhr = new XMLHttpRequest();
-xhr.open("get", "http://swapi.dev/api/planets/");
-xhr.send();
+const URL = 'https://swapi.dev/api/';
 
-xhr.addEventListener('error', (event) => {
-    console.log(event);
-});
-
-xhr.addEventListener('load', (e) => {
-    if (e.target.status === 200) {
-        console.log('OK');
-
-        const data = JSON.parse(e.target.response);
-        console.log(data)
-        data.results.forEach(item => {
-            const div = document.createElement("div");
-            div.classList.add("col", "card");
-            div.innerHTML = `
-                    <div class="card-body">
-                        <h5 class ="card-title">${item.name}</h5>
-                        <p class ="card-text">Created: ${item.created}</p>
-                    </div>
-                    <ul class ="list-group list-group-flush">
-                        <li class ="list-group-item"><b>Diameter:</b> ${item.diameter}</li>
-                        <li class ="list-group-item"><b>Orbital perioad:</b> ${item.orbital_period}</li>
-                        <li class ="list-group-item"><b>Population:</b> ${item.population}</li>
-                    </ul>
-                    <div class="card-body">
-                        <a href="${item.url}" class="card-link btn btn-primary">Planet link</a>
-                    </div>
-            `;
-            $container.append(div);
-            // console.log(data.results);
-            // console.log(typeof data.results);
-        })
-
-
-    } else if (e.target.status === 404) {
-        console.log("Not found")
+async function getPeople() {
+    const response = await fetch(URL + "people");
+    if (response.status !== 200) {
+        throw new Error("Error status")
     }
-});
-console.log(xhr)
+    console.log(response);
+    const people = await response.json();
+
+    console.log(people);
+}
+
+getPeople();
